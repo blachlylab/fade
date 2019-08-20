6e9198571cefea1136b53563d02b84c3aca20bc9
Charles Gregory <charles.gregory@osumc.edu>
2019-08-20 12:09:53 -0400

    actually making the correct change that was supposed to be addressed in 4306871

--- source/analysis.d
+++ source/analysis.d
@@ -58,6 +58,7 @@ Align_Result align_clip(bool left)(SAMReader * bam,string fai_f,Parasail * p,SAM
     res=p.sw_striped(q_seq,ref_seq);
 
     // ClipStatus clip = left ? status.left : status.right;
+    if((res.cigar.ops.length==0) | (res.cigar.ops.length>10)) return "";
     
     static if(left){
         if(res.cigar.ops[$-1].op==Ops.EQUAL){
