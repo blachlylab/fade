ab7e26302f322065517aa6635a694670dacef0ef
charlesgregory <charles.gregory940@topper.wku.edu>
2020-06-16 15:45:10 -0400

    bug fixes for recording bq of artifacts and reporting it in stats

--- source/stats.d
+++ source/stats.d
@@ -72,7 +72,7 @@ void statsfile(string[] args){
     import std.format:format;
     auto bam = SAMReader(args[1]);
     auto outfile = File(args[2],"w");
-    auto header = ["qname","rname","pos","cigar","art_start","art_end","aln_rname","aln_start","aln_end","art_cigar","stemloop","stemloop_rc","avgbq","art_avgbq","predicted_inverted_repeat","IR_identity","flagbinary","flag","strand"];
+    auto header = ["qname","rname","pos","cigar","art_start","art_end","aln_rname","aln_start","aln_end","art_cigar","stemloop","stemloop_rc","predicted_inverted_repeat","IR_identity","avgbq","art_avgbq","flagbinary","flag","strand"];
     outfile.writeln(header.join('\t'));
     auto ps = Parasail("ACTGN",2,-1,10,2);
     foreach(SAMRecord rec;bam.all_records()){
@@ -100,18 +100,19 @@ void statsfile(string[] args){
             towrite~=am_fields[2];
             towrite~=rec["as"].toString.splitter(";").front;
             towrite~=rec["ar"].toString.splitter(";").front;
-            towrite~=(float(rec.qscores!false().sum) / float(rec.length)).to!string;
-            towrite~=(float(rec["ab"].to!(ubyte[])[0..towrite[$-2].length].sum) / float(towrite[$-2].length)).to!string;
 
-            auto end = round(float(towrite[$-3].length) * 0.75).to!ulong;
+            auto end = round(float(towrite[$-2].length) * 0.75).to!ulong;
             parasail_query p;
-            p.seq1=toUTFz!(char *)(towrite[$-3][0..end]);
-            p.seq2=toUTFz!(char *)(towrite[$-2]);
+            p.seq1=toUTFz!(char *)(towrite[$-2][0..end]);
+            p.seq2=toUTFz!(char *)(towrite[$-1]);
             p.seq1Len=cast(int)end;
-            p.seq2Len=cast(int)towrite[$-2].length;
+            p.seq2Len=cast(int)towrite[$-1].length;
             p.result=ps.aligner!("sw","stats","striped","16")(p);
-            towrite~=towrite[$-3][0 .. p.result.end_query + 1];
+            towrite~=towrite[$-2][0 .. p.result.end_query + 1];
             towrite~=(float(parasail_result_get_matches(p.result)) / float(p.result.end_query + 1)).to!string;
+
+            towrite~=(float(rec.qscores!false().sum) / float(rec.length)).to!string;
+            towrite~=(float(rec["ab"].toString[0..towrite[$-4].length].sum) / float(towrite[$-4].length)).to!string;
 
             towrite~= format!"%08b"(rs.raw);
             towrite~= rs.raw.to!string;
@@ -135,19 +136,20 @@ void statsfile(string[] args){
             towrite~=am_fields[2];
             towrite~=rec["as"].toString.splitter(";").drop(1).front;
             towrite~=rec["ar"].toString.splitter(";").drop(1).front;
-            towrite~=(float(rec.qscores!false().sum) / float(rec.length)).to!string;
-            towrite~=(float(rec["ab"].to!(ubyte[])[$-towrite[$-2].length .. $].sum) / float(towrite[$-2].length)).to!string;
-            
-            auto end = round(float(towrite[$-3].length) * 0.75).to!ulong;
+           
+            auto end = round(float(towrite[$-2].length) * 0.75).to!ulong;
             parasail_query p;
-            p.seq1=toUTFz!(char *)(towrite[$-3][0..end]);
-            p.seq2=toUTFz!(char *)(towrite[$-2]);
+            p.seq1=toUTFz!(char *)(towrite[$-2][0..end]);
+            p.seq2=toUTFz!(char *)(towrite[$-1]);
             p.seq1Len=cast(int)end;
-            p.seq2Len=cast(int)towrite[$-2].length;
+            p.seq2Len=cast(int)towrite[$-1].length;
             p.result=ps.aligner!("sw","stats","striped","16")(p);
-            towrite~=towrite[$-3][0 .. p.result.end_query + 1];
+            towrite~=towrite[$-2][0 .. p.result.end_query + 1];
             towrite~=(float(parasail_result_get_matches(p.result)) / float(p.result.end_query + 1)).to!string;
 
+            towrite~=(float(rec.qscores!false().sum) / float(rec.length)).to!string;
+            towrite~=(float(rec["ab"].toString[$-towrite[$-4].length .. $].sum) / float(towrite[$-4].length)).to!string;
+ 
             towrite~= format!"%08b"(rs.raw);
             towrite~= rs.raw.to!string;
             // this is reverse of the normal strand function as the artifact
